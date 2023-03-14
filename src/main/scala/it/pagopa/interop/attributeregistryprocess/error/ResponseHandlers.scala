74cb84acf91841687768ea878ecd7319649b2317
Fabrizio Musella <fabrizio.musella@pagopa.it>
2023-03-14 15:40:58 +0100

    Removed unused responseHandler

--- src/main/scala/it/pagopa/interop/attributeregistryprocess/error/ResponseHandlers.scala
+++ src/main/scala/it/pagopa/interop/attributeregistryprocess/error/ResponseHandlers.scala
@@ -77,12 +77,4 @@ object ResponseHandlers extends AkkaResponses {
       case Failure(ex: OperationForbidden.type) => forbidden(ex, logMessage)
       case Failure(ex)                          => internalServerError(ex, logMessage)
     }
-
-  def loadCertifiedAttributesResponse[T](logMessage: String)(
-    success: T => Route
-  )(result: Try[T])(implicit contexts: Seq[(String, String)], logger: LoggerTakingImplicit[ContextFieldsToLog]): Route =
-    result match {
-      case Success(s)  => success(s)
-      case Failure(ex) => internalServerError(ex, logMessage)
-    }
 }

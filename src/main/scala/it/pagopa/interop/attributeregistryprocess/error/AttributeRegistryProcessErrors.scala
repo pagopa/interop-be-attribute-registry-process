9ae6ed545b392f8fbe3f689d5f1b62dd6be2181c
Fabrizio Musella <fabrizio.musella@pagopa.it>
2023-03-15 09:57:35 +0100

    Cleaned errors not related to the service and corrected its name in CI

--- src/main/scala/it/pagopa/interop/attributeregistryprocess/error/AttributeRegistryProcessErrors.scala
+++ src/main/scala/it/pagopa/interop/attributeregistryprocess/error/AttributeRegistryProcessErrors.scala
@@ -5,42 +5,7 @@ import it.pagopa.interop.commons.utils.errors.ComponentError
 import java.util.UUID
 
 object AttributeRegistryProcessErrors {
-  final case class TenantIsNotACertifier(tenantId: UUID)
-      extends ComponentError("0001", s"Organization ${tenantId.toString} not allowed to assign attributes")
-
-  final case class SelfcareIdConflict(tenantId: UUID, existingSelfcareId: String, newSelfcareId: String)
-      extends ComponentError(
-        "0002",
-        s"Conflict on Tenant SelfCare ID update for tenant $tenantId: old value $existingSelfcareId - new value $newSelfcareId"
-      )
-
-  final case class DeclaredAttributeNotFound(tenantId: UUID, attributeId: String)
-      extends ComponentError("0003", s"Attribute $attributeId not found for Tenant $tenantId")
-
-  object VerifiedAttributeSelfVerification
-      extends ComponentError("0004", s"Organizations are not allowed to verify own attributes")
-
-  final case class AttributeVerificationNotAllowed(consumerId: UUID, attributeId: UUID)
-      extends ComponentError(
-        "0005",
-        s"Organization is not allowed to verify attribute $attributeId for tenant $consumerId"
-      )
-
-  final case class AttributeAlreadyRevoked(targetTenantId: UUID, requesterTenantId: UUID, attributeId: UUID)
-      extends ComponentError(
-        "0006",
-        s"Attribute $attributeId has been already revoked for $targetTenantId by $requesterTenantId"
-      )
-
-  object VerifiedAttributeSelfRevocation
-      extends ComponentError("0007", s"Organizations are not allowed to revoke own attributes")
-
-  final case class AttributeRevocationNotAllowed(consumerId: UUID, attributeId: UUID)
-      extends ComponentError(
-        "0008",
-        s"Organization is not allowed to revoke attribute $attributeId for tenant $consumerId"
-      )
 
   final case class RegistryAttributeNotFound(attributeIdentifier: String)
-      extends ComponentError("0009", s"Attribute $attributeIdentifier not found in registry")
+      extends ComponentError("0001", s"Attribute $attributeIdentifier not found in registry")
 }

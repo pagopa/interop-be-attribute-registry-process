6a6e701403b3aafea643a27bbdc745589bff4c32
Stefano Perazzolo <13318704+beetlecrunch@users.noreply.github.com>
2023-07-03 16:48:04 +0200

    Revert "Update PagopaVersions"
    This reverts commit 36b7985a384a27f873331c8413cfdd84e8cf54aa.

--- project/Versions.scala
+++ project/Versions.scala
@@ -19,7 +19,7 @@ object Versions {
 }
 
 object PagopaVersions {
-  lazy val attributeRegistryManagementVersion = "1.0.12"
-  lazy val commonsVersion                     = "1.0.18"
-  lazy val partyProxyVersion                  = "1.0.11"
+  lazy val attributeRegistryManagementVersion = "1.0.x-SNAPSHOT"
+  lazy val commonsVersion                     = "1.0.x-SNAPSHOT"
+  lazy val partyProxyVersion                  = "1.0.x-SNAPSHOT"
 }
